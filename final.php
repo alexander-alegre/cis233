6491ce3937dc5a323fc63d41d4a30fe1641f5e57
= <alexander.alegre@outlook.com>
2017-04-26 13:54:27 -0700

    add code for links in final project

--- final.php
+++ final.php
@@ -4,7 +4,10 @@ $custom_css = '<link rel="stylesheet" type="text/css" href="resources/css/">';
 require('inc/header.php') 
 ?>
 	<h1>Final Project</h1>
-
+	<div class="container">
+		<a href="https://github.com/alexander-alegre/CIS-Community" class="btn btn-primary">Code</a>
+		<a href="http://ciscom.herokuapp.com/" class="btn btn-primary">App</a>
+	</div>
 
 <?php 
 $custom_js = '<script src="resources/js/" type="text/javascript"></script>';
--- rollover.php
+++ rollover.php
@@ -4,13 +4,14 @@ $custom_css = '<link rel="stylesheet" type="text/css" href="resources/css/">';
 require('inc/header.php') 
 ?>
 	<h1>Rollover Page</h1>
-	<a href="https://github.com/alexander-alegre/node-chat-app">
-		<p>Code</p>
-	</a>
-	<a href="https://quiet-plains-47973.herokuapp.com/">
-		<p>app</p>
-	</a>
-
+	<div class="container">
+		<a class="btn btn-primary" href="https://github.com/alexander-alegre/node-chat-app">
+			Code
+		</a>
+		<a class="btn btn-primary" href="https://quiet-plains-47973.herokuapp.com/">
+			App
+		</a>
+	</div>
 <?php 
 $custom_js = '<script src="resources/js/" type="text/javascript"></script>';
 require('inc/footer.php') 
